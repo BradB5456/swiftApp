775688ed68a0271f5f851c5c91c8c96b7ff6da83
brad blake <bradblake@brads-imac.bigpond>
2016-08-25 17:09:26 +1000

    Initial Commit

---
+++ swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift App
+//
+//  Created by brad blake on 25/08/2016.
+//  Copyright © 2016 brad blake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
